eea6d569eca9d66d829520c5ad0892b3619248eb
Lewis <incursion45@live.com>
2020-08-30 16:57:35 -0400

    last check in for now

--- main.asm
+++ main.asm
@@ -1,12 +1,15 @@
 mov ah, 0x0e
-mov al, 65
-mov dh, 1
+mov al, 97
+mov dh, 0
 int 0x10
 
+jmp loop
+
+;labels
 add32:
     inc al ; adding 1 to al so loop can move forward
     add al, 32 ; adding 32 to make lowercase
-    cmp al , 'Z' + 1 ; make sure we in bounds
+    cmp al , 'z' + 1 ; make sure we in bounds
     je exit ; if we not exit
     mov dh, 0 ; set case toggle
     jmp jumpLoop ; jump back to loop
@@ -14,7 +17,7 @@ add32:
 sub32: 
     inc al ; adding 1 to al so loop can move forward
     sub al, 32 ; subtract 32 to make uppercase
-    cmp al , 'Z' + 1 ; bounds check
+    cmp al , 'z' + 1 ; bounds check
     je exit ; leaves if out of bounds 
     mov dh, 1 ; set case toogle
     jmp jumpLoop
